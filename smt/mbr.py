Added several tests; fixed a bug in MBR to make it work in derivative training data

smt/mbr.py
@@ -93,14 +93,14 @@ class MBR(SM):
             nnz = nt * num['order']
             data, rows, cols = MBRlib.compute_jac(kx, 0, nx, nt, nnz,
                 num['order_list'], num['ctrl_list'], t)
-            if kx != 0:
+            if kx > 0:
                 data /= xlimits[kx-1, 1] - xlimits[kx-1, 0]
 
             rect_mtx = scipy.sparse.csc_matrix((data, (rows, cols)),
                 shape=(nt, num['ctrl']))
 
             mtx = mtx + rect_mtx.T * rect_mtx
-            rhs = rect_mtx.T * yt
+            rhs += rect_mtx.T * yt
 
         diag = sm_options['reg'] * np.ones(num['ctrl'])
         arange = np.arange(num['ctrl'])

smt/problems/tensor_product.py
@@ -22,16 +22,16 @@ class TensorProduct(Problem):
 
         if self.options['func'] == 'cos':
             a = self.options['width']
-            self.func = lambda v: np.cos(a * 2 * np.pi * v)
-            self.dfunc = lambda v: -a * 2 * np.pi * np.sin(a * 2 * np.pi * v)
+            self.func = lambda v: np.cos(a * np.pi * v)
+            self.dfunc = lambda v: -a * np.pi * np.sin(a * np.pi * v)
         elif self.options['func'] == 'exp':
             a = self.options['width']
-            self.func = lambda v: np.exp(5 * a * v)
-            self.dfunc = lambda v: 5 * a * np.exp(5 * a * v)
+            self.func = lambda v: np.exp(a * v)
+            self.dfunc = lambda v: a * np.exp(a * v)
         elif self.options['func'] == 'tanh':
             a = self.options['width']
-            self.func = lambda v: np.tanh(5 * a * v)
-            self.dfunc = lambda v: 5 * a / np.cosh(5 * a * v) ** 2
+            self.func = lambda v: np.tanh(a * v)
+            self.dfunc = lambda v: a / np.cosh(a * v) ** 2
 
     def _evaluate(self, x, kx):
         """
@@ -40,7 +40,7 @@ class TensorProduct(Problem):
         x : ndarray[ne, nx]
             Evaluation points.
         kx : int or None
-            Index of derivative to return values with respect to.
+            Index of derivative (0-based) to return values with respect to.
             None means return function value rather than derivative.
 
         Returns

smt/sm.py
@@ -32,8 +32,10 @@ class SM(object):
         '''
         #Initialization
         self._set_default_options()
-        self.sm_options.update(sm_options)
-        self.printf_options.update(printf_options)
+        if sm_options is not None:
+            self.sm_options.update(sm_options)
+        if printf_options is not None:
+            self.printf_options.update(printf_options)
 
         self.training_pts = {'exact': {}}
 
@@ -56,11 +58,10 @@ class SM(object):
         elif xe is None and ye is None:
             num = 0.
             den = 0.
-            for kx in self.training_pts['exact']:
-                xt, yt = self.training_pts['exact'][kx]
-                yt2 = self.predict(xt)
-                num += np.linalg.norm(yt2 - yt) ** 2
-                den += np.linalg.norm(yt) ** 2
+            xt, yt = self.training_pts['exact'][0]
+            yt2 = self.predict(xt)
+            num += np.linalg.norm(yt2 - yt) ** 2
+            den += np.linalg.norm(yt) ** 2
             return num ** 0.5 / den ** 0.5
 
     def add_training_pts(self, typ, xt, yt, kx=None):