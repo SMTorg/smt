1da9e379d29146ed933d3896929cc8d4defadac1
John Hwang <hwangjt@umich.edu>
2017-03-02 12:02:02 -0500

    Actually added the new file for RMT

---
+++ smt/rmt.py
@@ -0,0 +1,105 @@
+"""
+Author: Dr. John T. Hwang <hwangjt@umich.edu>
+"""
+from __future__ import division
+
+import numpy as np
+import scipy.sparse
+from six.moves import range
+
+import RMTSlib
+
+from smt.utils.linear_solvers import get_solver
+from smt.utils.line_search import get_line_search_class
+from smt.utils.caching import _caching_checksum_sm, _caching_load, _caching_save
+from smt.utils.sparse import assemble_sparse_mtx
+from smt.sm import SM
+
+
+class RMT(SM):
+    """
+    Regularized Minimal-energy Tensor-product interpolant base class for RMTS and RMTB.
+    """
+
+    def fit(self):
+        """
+        Train the model
+        """
+        checksum = _caching_checksum_sm(self)
+
+        filename = '%s.sm' % self.sm_options['name']
+        success, data = _caching_load(filename, checksum)
+        if not success or not self.sm_options['save_solution']:
+            self._fit()
+            data = {'sol': self.sol, 'num': self.num}
+            _caching_save(filename, checksum, data)
+        else:
+            self.sol = data['sol']
+            self.num = data['num']
+
+    def evaluate(self, x, kx):
+        """
+        Evaluate the surrogate model at x.
+
+        Parameters
+        ----------
+        x : np.ndarray[n_eval,dim]
+            An array giving the point(s) at which the prediction(s) should be made.
+        kx : int or None
+            None if evaluation of the interpolant is desired.
+            int  if evaluation of derivatives of the interpolant is desired
+                 with respect to the kx^{th} input variable (kx is 0-based).
+
+        Returns
+        -------
+        y : np.ndarray[n_eval,1]
+            - An array with the output values at x.
+        """
+        n = x.shape[0]
+
+        num = self.num
+        sm_options = self.sm_options
+
+        data, rows, cols = self._compute_jac(kx, 0, x)
+
+        # In the explanation below, n is the number of dimensions, and
+        # a_k and b_k are the lower and upper bounds for x_k.
+        #
+        # A C1 extrapolation can get very tricky, so we implement a simple C0
+        # extrapolation. We basically linarly extrapolate from the nearest
+        # domain point. For example, if n = 4 and x2 > b2 and x3 > b3:
+        #    f(x1,x2,x3,x4) = f(x1,b2,b3,x4) + dfdx2 (x2-b2) + dfdx3 (x3-b3)
+        #    where the derivatives are evaluated at x1,b2,b3,x4 (called b) and
+        #    dfdx1|x = dfdx1|b + d2fdx1dx2|b (x2-b2) + d2fdx1dx3|b (x3-b3)
+        #    dfdx2|x = dfdx2|b.
+        # The dfdx2|x derivative is what it is because f and all derivatives
+        # evaluated at x1,b2,b3,x4 are constant with respect to changes in x2.
+        # On the other hand, the dfdx1|x derivative is what it is because
+        # f and all derivatives evaluated at x1,b2,b3,x4 change with x1.
+        # The extrapolation function is non-differentiable at boundaries:
+        # i.e., where x_k = a_k or x_k = b_k for at least one k.
+        if sm_options['extrapolate']:
+
+            # First we evaluate the vector pointing to each evaluation points
+            # from the nearest point on the domain, in a matrix called dx.
+            # If the ith evaluation point is not external, dx[i, :] = 0.
+            ndx = n * num['support']
+            dx = RMTSlib.compute_ext_dist(num['x'], n, ndx, sm_options['xlimits'], x)
+            isexternal = np.array(np.array(dx, bool), float)
+
+            for ix in range(num['x']):
+                # Now we compute the first order term where we have a
+                # derivative times (x_k - b_k) or (x_k - a_k).
+                data_tmp, rows, cols = self._compute_jac(kx, ix+1, x)
+                data_tmp *= dx[:, ix]
+
+                # If we are evaluating a derivative (with index kx),
+                # we zero the first order terms for which dx_k = 0.
+                if kx != 0:
+                    data_tmp *= 1 - isexternal[:, kx-1]
+
+                data += data_tmp
+
+        mtx = scipy.sparse.csc_matrix((data, (rows, cols)), shape=(n, num['dof']))
+
+        return mtx.dot(self.sol)
