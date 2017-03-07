047d6c9ec8b27052551e1e508ac27d808b81b4af
John Hwang <hwangjt@Johns-MacBook.local>
2017-03-07 12:36:27 -0500

    Undid the previous setup.py change related to Fortran source compilation on Windows

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from numpy.distutils.core import setup, Extension
 import os
 
 
-if os.name == 'nt' and 0:
+if os.name == 'nt':
     # If OS is Windows, don't compile Fortran code
     ext = []
 else:
