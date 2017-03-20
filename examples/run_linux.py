961b29c312a902d0acc935ae609388eaa8bab05f
bouhlelma <mbouhlel@umich.edu>
2017-03-20 14:33:45 -0400

    - clean up compiled_available for t_errors and e_erros in test_all - change kriging error to 1e-5 in test_all - Add the ese criterion in the sampling class - generating training points using the smt.sampling and smt.problems in run_lunix and run windows

--- examples/run_linux.py
+++ examples/run_linux.py
@@ -5,9 +5,8 @@ from smt.pa2 import PA2
 from smt.kpls import KPLS
 from smt.idw import IDW
 from scipy import linalg
-import tools_benchmark as fun
-from tools_doe import  trans
-import doe_lhs
+from smt.problems import Carre
+from smt.sampling import LHS
 
 # Initialization of the problem
 dim = 10
@@ -18,27 +17,29 @@ xlimits = np.zeros((dim, 2))
 xlimits[:, 0] = -10
 xlimits[:, 1] = 10
 
-# Construction of the DOE in [0,1]
-xt = doe_lhs.lhs(dim,ndoe,'m')
-
-# Transform the DOE in [LB,UB]
-xt = trans(xt,xlimits[:, 0],xlimits[:, 1])
+# Construction of the DOE
+sampling = LHS(xlimits=xlimits,criterion = 'm')
+xt = sampling(ndoe)
 
 # Compute the output (+ the gradient)
-yt,yd = fun.carre(xt)
+fun = Carre(ndim = dim)
+yt = fun(xt)
+for i in range(dim):
+    yd = fun(xt,kx=i)
+    yt = np.concatenate((yt,yd),axis=1)
 
 # Construction of the validation points
 ntest = 500
-xtest = doe_lhs.lhs(dim, ntest)
-xtest = trans(xtest,xlimits[:, 0],xlimits[:, 1])
-ytest,ydtest = fun.carre(xtest)
+sampling = LHS(xlimits=xlimits,criterion = 'm')
+xtest = sampling(ntest)
+ytest = fun(xtest)
 
 ########### The LS model
 
 # Initialization of the model
 t = LS()
 # Add the DOE
-t.add_training_pts('exact',xt,yt)
+t.add_training_pts('exact',xt,yt[:,0])
 # Train the model
 t.train()
 # Prediction of the validation points
@@ -49,7 +50,7 @@ print('LS,  err: '+str(linalg.norm(y.reshape((ntest,1))-ytest.reshape((ntest,
 
 ########### The PA2 model
 t = PA2()
-t.add_training_pts('exact',xt,yt)
+t.add_training_pts('exact',xt,yt[:,0])
 t.train()
 y = t.predict(xtest)
 
@@ -58,7 +59,7 @@ print('PA2,  err: '+str(linalg.norm(y.reshape((ntest,1))-ytest.reshape((ntest,
 
 ########### The IDW model
 t = IDW()
-t.add_training_pts('exact',xt,yt)
+t.add_training_pts('exact',xt,yt[:,0])
 t.train()
 y = t.predict(xtest)
 
@@ -69,7 +70,7 @@ print('IDW,  err: '+str(linalg.norm(y.reshape((ntest,1))-ytest.reshape((ntest,
 # The variables 'name', 'ncomp' and 'theta0' must be equal to 'Kriging',
 # dim and a list of length dim, respectively.
 t = KPLS(name='KRG', n_comp=dim, theta0=[1e-2]*dim)
-t.add_training_pts('exact',xt,yt)
+t.add_training_pts('exact',xt,yt[:,0])
 
 t.train()
 y = t.predict(xtest)
@@ -83,7 +84,7 @@ print('Kriging,  err: '+str(linalg.norm(y.reshape((ntest,1))-ytest.reshape((ntes
 # an integer in [1,dim[ and a list of length n_comp, respectively. Here is an
 # an example using 1 principal component.
 t = KPLS(name='KPLS', n_comp=2, theta0=[1e-2,1e-2])
-t.add_training_pts('exact',xt,yt)
+t.add_training_pts('exact',xt,yt[:,0])
 
 t.train()
 y = t.predict(xtest)
@@ -96,7 +97,7 @@ print('KPLS,  err: '+str(linalg.norm(y.reshape((ntest,1))-ytest.reshape((ntest,
 # The variables 'name' must be equal to 'KPLSK'. 'n_comp' and 'theta0' must be
 # an integer in [1,dim[ and a list of length n_comp, respectively.
 t = KPLS(name='KPLSK', n_comp=2, theta0=[1e-2,1e-2])
-t.add_training_pts('exact',xt,yt)
+t.add_training_pts('exact',xt,yt[:,0])
 t.train()
 y = t.predict(xtest)
 
@@ -106,11 +107,11 @@ print('KPLSK,  err: '+str(linalg.norm(y.reshape((ntest,1))-ytest.reshape((ntest,
 ########### The GEKPLS model
 # The variables 'name' must be equal to 'GEKPLS'. 'n_comp' and 'theta0' must be
 # an integer in [1,dim[ and a list of length n_comp, respectively.
-t = KPLS(name='GEKPLS', n_comp=2, theta0=[1e-2,1e-2], xlimits=xlimits)
-t.add_training_pts('exact',xt,yt)
+t = KPLS(name='GEKPLS', n_comp=1, theta0=[1e-2], xlimits=xlimits)
+t.add_training_pts('exact',xt,yt[:,0])
 # Add the gradient information
 for i in range(dim):
-    t.add_training_pts('exact',xt,yd[:, i].reshape((yt.shape[0],1)),kx=i)
+    t.add_training_pts('exact',xt,yt[:, 1+i].reshape((yt.shape[0],1)),kx=i)
 
 t.train()
 y = t.predict(xtest)
