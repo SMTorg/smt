bd085053781e68b21a3abe4bb3be9d8c9ea80d81
bouhlelma <mbouhlel@umich.edu>
2017-09-07 10:07:44 -0400

    Include logo in the documentation

--- doc/conf.py
+++ doc/conf.py
@@ -94,12 +94,15 @@ html_theme = 'bizstyle'
 # further.  For a list of options available for each theme, see the
 # documentation.
 #
-# html_theme_options = {}
+#html_theme_options = {}
+
+html_logo = 'logos.png'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
 
 
 # -- Options for HTMLHelp output ------------------------------------------
