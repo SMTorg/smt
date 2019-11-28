fixed VFM derivative computation for array outputs and added bool to assure that train is only called once

smt/applications/vfm.py
@@ -16,6 +16,15 @@ from smt.applications.application import SurrogateBasedApplication
 
 
 class VFM(SurrogateBasedApplication):
+
+
+    def __init__(self, **kwargs):
+        super(VFM, self).__init__(**kwargs)
+
+        self.nx = self.options['X_LF'].shape[1]
+        self.ny = self.options['y_LF'].shape[1]
+        self._trained = False
+
     def _initialize(self):
         super(VFM, self)._initialize()
         declare = self.options.declare
@@ -88,7 +97,8 @@ class VFM(SurrogateBasedApplication):
             Output values at the prediction points.
 
         """
-        self._apply()
+        if not self._trained:
+            self._apply()
         y = self.sm_HF["predict_values"](x)
         return y
 
@@ -110,13 +120,14 @@ class VFM(SurrogateBasedApplication):
             Derivatives at the prediction points.
 
         """
-        self._apply()
+        if not self._trained:
+            self._apply()
         if kx is None:
             y = np.zeros(x.shape)
             for i in range(x.shape[1]):
                 y[:, i] = self.sm_HF["predict_derivatives"][i](x).reshape((x.shape[0]))
         else:
-            y = self.sm_HF["predict_derivatives"][kx](x).reshape((x.shape[0], 1))
+            y = self.sm_HF["predict_derivatives"][kx](x).reshape((x.shape[0], self.ny))
 
         return y
 
@@ -124,6 +135,8 @@ class VFM(SurrogateBasedApplication):
         """
         Algorithm of the VFM method
         """
+
+        self._trained = True
 
         # For seek of readability
         if (