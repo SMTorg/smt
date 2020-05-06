2628ef93ac1ee2f63298667837baca0ee0843c4d
relf <remi.lafage@onera.fr>
2020-05-06 15:57:04 +0200

    merge ego/egop wip

--- smt/applications/ego.py
+++ smt/applications/ego.py
@@ -1,10 +1,9 @@
 """
-Author: Remi Lafage <remi.lafage@onera.fr> and Nathalie Bartoli
+Authors: Nathalie Bartoli, Remy Priem, Remi Lafage, Emile Roux <emile.roux@univ-smb.fr>
 
 This package is distributed under New BSD license.
 
 """
-# TODO : documentation
 
 from __future__ import division
 import six
@@ -46,6 +45,19 @@ class EGO(SurrogateBasedApplication):
         )
         declare("n_start", 20, types=int, desc="Number of optimization start points")
         declare(
+            "n_par",
+            1,
+            types=int,
+            desc="Number of parallel sample to compute using the qEI ",
+        )
+        declare(
+            "qEIAproxCrit",
+            "KBLB",
+            types=str,
+            values=["KB", "KBLB", "KBUB", "KBRand", "CLmin"],
+            desc="Approximated q-EI maximization strategy ",
+        )
+        declare(
             "n_doe",
             None,
             types=int,
@@ -76,7 +88,10 @@ class EGO(SurrogateBasedApplication):
         [ndoe, nx]: coord-x initial doe
         [ndoe, 1]: coord-y initial doe
         """
+        # Set the bounds of the optimization problem
         xlimits = self.options["xlimits"]
+
+        # Build initial DOE
         sampling = LHS(xlimits=xlimits, criterion="ese")
 
         xdoe = self.options["xdoe"]
@@ -91,7 +106,7 @@ class EGO(SurrogateBasedApplication):
         ydoe = self.options["ydoe"]
         if ydoe is None:
             y_doe = fun(x_doe)
-        else: # to save time if y_doe is already given to EGO
+        else:  # to save time if y_doe is already given to EGO
             y_doe = ydoe
 
         # to save the initial doe
@@ -106,58 +121,36 @@ class EGO(SurrogateBasedApplication):
         n_iter = self.options["n_iter"]
         n_start = self.options["n_start"]
         n_max_optim = self.options["n_max_optim"]
+        n_par = self.options["n_par"]
 
         for k in range(n_iter):
-
-            self.gpr.set_training_values(x_data, y_data)
-            self.gpr.train()
-
-            if criterion == "EI":
-                self.obj_k = lambda x: -self.EI(np.atleast_2d(x), y_data)
-            elif criterion == "SBO":
-                self.obj_k = lambda x: self.SBO(np.atleast_2d(x))
-            elif criterion == "UCB":
-                self.obj_k = lambda x: self.UCB(np.atleast_2d(x))
-
-            success = False
-            n_optim = 1  # in order to have some success optimizations with SLSQP
-            while not success and n_optim <= n_max_optim:
-                opt_all = []
-                x_start = sampling(n_start)
-                for ii in range(n_start):
-                    opt_all.append(
-                        minimize(
-                            self.obj_k,
-                            x_start[ii, :],
-                            method="SLSQP",
-                            bounds=bounds,
-                            options={"maxiter": 200},
+            # Virtual enrichement loop
+            for p in range(n_par):
+                x_et_k, success = self._find_points(x_data, y_data)
+                if not success:
+                    self.log(
+                        "Internal optimization failed at EGO iter = {}.{}".format(k, p)
+                    )
+                    break
+                elif success:
+                    self.log(
+                        "Internal optimization succeeded at EGO iter = {}.{}".format(
+                            k, p
                         )
                     )
+                # Set temporaly the y_data to the one predicted by the kringin metamodel
+                y_et_k = self.set_virtual_point(np.atleast_2d(x_et_k), y_data)
 
-                opt_all = np.asarray(opt_all)
+                # Update y_data with predicted value
+                y_data = np.atleast_2d(np.append(y_data, y_et_k)).T
+                x_data = np.atleast_2d(np.append(x_data, x_et_k, axis=0))
 
-                opt_success = opt_all[[opt_i["success"] for opt_i in opt_all]]
-                obj_success = np.array([opt_i["fun"] for opt_i in opt_success])
-                success = obj_success.size != 0
-                if not success:
-                    self.log("New start point for the internal optimization")
-                    n_optim += 1
+            # Compute the real values of y_data
+            x_to_compute = np.atleast_2d(x_data[-n_par:])
+            y = fun(x_to_compute)
+            y_data[-n_par:] = y
 
-            if n_optim >= n_max_optim:
-                self.log("Internal optimization failed at EGO iter = {}".format(k))
-                break
-            elif success:
-                self.log("Internal optimization succeeded at EGO iter = {}".format(k))
-
-            ind_min = np.argmin(obj_success)
-            opt = opt_success[ind_min]
-            x_et_k = np.atleast_2d(opt["x"])
-            y_et_k = fun(x_et_k)
-
-            y_data = np.atleast_2d(np.append(y_data, y_et_k)).T
-            x_data = np.atleast_2d(np.append(x_data, x_et_k, axis=0))
-
+        # Find the optimal point
         ind_best = np.argmin(y_data)
         x_opt = x_data[ind_best]
         y_opt = y_data[ind_best]
@@ -193,3 +186,87 @@ class EGO(SurrogateBasedApplication):
         var = self.gpr.predict_variances(point)
         res = pred - 3.0 * np.sqrt(var)
         return res
+
+    def _find_points(self, x_data, y_data):
+        """
+        Function that analyse a set of x_data and y_data and give back the 
+        more interesting point to evaluates according to the selected criterion
+        
+        Inputs: 
+            - x_data and y_data
+        Outputs:
+            - x_et_k : the points to evaluate
+            - success bool : boolean succes flag to interupte
+                the main loop if need
+        
+        """
+        self.gpr.set_training_values(x_data, y_data)
+        self.gpr.train()
+
+        criterion = self.options["criterion"]
+        n_start = self.options["n_start"]
+        n_max_optim = self.options["n_max_optim"]
+        bounds = self.options["xlimits"]
+
+        if criterion == "EI":
+            self.obj_k = lambda x: -self.EI(np.atleast_2d(x), y_data)
+        elif criterion == "SBO":
+            self.obj_k = lambda x: self.SBO(np.atleast_2d(x))
+        elif criterion == "UCB":
+            self.obj_k = lambda x: self.UCB(np.atleast_2d(x))
+
+        success = False
+        n_optim = 1  # in order to have some success optimizations with SLSQP
+        while not success and n_optim <= n_max_optim:
+            opt_all = []
+            x_start = self.sampling(n_start)
+            for ii in range(n_start):
+                opt_all.append(
+                    minimize(
+                        self.obj_k,
+                        x_start[ii, :],
+                        method="SLSQP",
+                        bounds=bounds,
+                        options={"maxiter": 200},
+                    )
+                )
+
+            opt_all = np.asarray(opt_all)
+
+            opt_success = opt_all[[opt_i["success"] for opt_i in opt_all]]
+            obj_success = np.array([opt_i["fun"] for opt_i in opt_success])
+            success = obj_success.size != 0
+            if not success:
+                self.log("New start point for the internal optimization")
+                n_optim += 1
+
+        if n_optim >= n_max_optim:
+            # self.log("Internal optimization failed at EGO iter = {}".format(k))
+            return np.atleast_2d(0), False
+
+        ind_min = np.argmin(obj_success)
+        opt = opt_success[ind_min]
+        x_et_k = np.atleast_2d(opt["x"])
+        return x_et_k, True
+
+    def set_virtual_point(self, x, y_data):
+        qEIAproxCrit = self.options["qEIAproxCrit"]
+
+        if qEIAproxCrit == "CLmin":
+            return np.min(y_data)
+
+        if qEIAproxCrit == "KB":
+            return self.gpr.predict_values(x)
+
+        if qEIAproxCrit == "KBUB":
+            conf = 3.0
+
+        if qEIAproxCrit == "KBLB":
+            conf = -3.0
+
+        if qEIAproxCrit == "KBRand":
+            conf = np.random.randn()
+
+        pred = self.gpr.predict_values(x)
+        var = self.gpr.predict_variances(x)
+        return pred + conf * np.sqrt(var)
