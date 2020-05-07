c3ada60b1acea205037392f19fe56f48bf0b6322
relf <remi.lafage@onera.fr>
2020-05-07 12:03:22 +0200

    expose NestedLHS, Evaluator

--- smt/applications/__init__.py
+++ smt/applications/__init__.py
@@ -1,6 +1,6 @@
 from .vfm import VFM
 from .moe import MOE
-from .mfk import MFK
+from .mfk import MFK, NestedLHS
 from .mfkpls import MFKPLS
 from .mfkplsk import MFKPLSK
-from .ego import EGO
+from .ego import EGO, Evaluator
