Fixed errors in the variance computation in mfk.py : - correction of sigma2, which is already descaled after _reduced_likelihood_function,     and was descaled a second time in MFK - correction of MSE line 487, should be Q_/(self.nt_all[i] - p - q)     instead of Q_/(2*(self.nt_all[i] - p - q)) ; moreover, sigma2 = Q_/(self.nt_all[i] - p - q),     so it is useless to compute Q_, and the expression is rewritten with sigma2 as in Le Gratiet and Cannamela 2015 Added a new test for the variance computation with mfk

smt/applications/mfk.py
@@ -5,6 +5,13 @@ Created on Fri May 04 10:26:49 2018
 @author: Mostafa Meliani <melimostafa@gmail.com>
 Multi-Fidelity co-Kriging: recursive formulation with autoregressive model of
 order 1 (AR1)
+
+Modified by Vincent Drouet :
+- correction of sigma2, which is already descaled after _reduced_likelihood_function,
+    and was descaled a second time in MFK
+- correction of MSE line 487, should be Q_/(self.nt_all[i] - p - q)
+    instead of Q_/(2*(self.nt_all[i] - p - q)) ; moreover, sigma2 = Q_/(self.nt_all[i] - p - q),
+    so it is useless to compute Q_, and the expression is rewritten with sigma2 as in Le Gratiet and Cannamela 2015
 """
 
 from __future__ import division
@@ -262,9 +269,11 @@ class MFK(KrgBased):
             self.nt = self.nt_all[lvl]
             self.q = self.q_all[lvl]
             self.p = self.p_all[lvl]
-            self.optimal_rlf_value[lvl], self.optimal_par[lvl], self.optimal_theta[
-                lvl
-            ] = self._optimize_hyperparam(D)
+            (
+                self.optimal_rlf_value[lvl],
+                self.optimal_par[lvl],
+                self.optimal_theta[lvl],
+            ) = self._optimize_hyperparam(D)
             if self.options["eval_noise"]:
                 tmp_list = self.optimal_theta[lvl]
                 self.optimal_theta[lvl] = tmp_list[:-1]
@@ -352,8 +361,7 @@ class MFK(KrgBased):
 
         # scaled predictor
         if descale:
-            for i in range(lvl):  # Predictor
-                mu[:, i] = self.y_mean + self.y_std * mu[:, i]
+            mu = mu * self.y_std + self.y_mean
 
         return mu[:, -1].reshape((n_eval, 1))
 
@@ -439,7 +447,8 @@ class MFK(KrgBased):
         G = self.optimal_par[0]["G"]
 
         u_ = solve_triangular(G.T, f.T - np.dot(Ft.T, r_t), lower=True)
-        MSE[:, 0] = self.optimal_par[0]["sigma2"] * (
+        sigma2 = self.optimal_par[0]["sigma2"] / self.y_std ** 2
+        MSE[:, 0] = sigma2 * (
             1 + self.noise[0] - (r_t ** 2).sum(axis=0) + (u_ ** 2).sum(axis=0)
         )
 
@@ -462,10 +471,10 @@ class MFK(KrgBased):
             beta = self.optimal_par[i]["beta"]
 
             # scaled predictor
-            sigma2 = self.optimal_par[i]["sigma2"]
+            sigma2 = self.optimal_par[i]["sigma2"] / self.y_std ** 2
             q = self.q_all[i]
-            p = self.p_all[i]
-            Q_ = (np.dot((yt - np.dot(Ft, beta)).T, yt - np.dot(Ft, beta)))[0, 0]
+            # p = self.p_all[i]
+            # Q_ = (np.dot((yt - np.dot(Ft, beta)).T, yt - np.dot(Ft, beta)))[0, 0]
             u_ = solve_triangular(G.T, f - np.dot(Ft.T, r_t), lower=True)
             sigma2_rho = np.dot(
                 g,
@@ -475,17 +484,12 @@ class MFK(KrgBased):
             sigma2_rho = (sigma2_rho * g).sum(axis=1)
             sigma2_rhos.append(sigma2_rho)
 
-            MSE[:, i] = (
-                sigma2_rho * MSE[:, i - 1]
-                + Q_
-                / (2 * (self.nt_all[i] - p - q))
-                * (1 + self.noise[i] - (r_t ** 2).sum(axis=0))
-                + sigma2 * (u_ ** 2).sum(axis=0)
+            MSE[:, i] = sigma2_rho * MSE[:, i - 1] + sigma2 * (
+                1 + self.noise[i] - (r_t ** 2).sum(axis=0) + (u_ ** 2).sum(axis=0)
             )
 
         # scaled predictor
-        for i in range(nlevel):  # Predictor
-            MSE[:, i] = self.y_std ** 2 * MSE[:, i]
+        MSE *= self.y_std ** 2
 
         return MSE, sigma2_rhos
 