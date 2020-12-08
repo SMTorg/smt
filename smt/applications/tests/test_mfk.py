5b86d0e7cdbb9b1b45255d503e620478199b9eeb
anfelopera <anfelopera@utp.edu.co>
2020-12-08 15:26:14 +0100

    fixing test MFK according to new devs

--- smt/applications/tests/test_mfk.py
+++ smt/applications/tests/test_mfk.py
@@ -169,7 +169,7 @@ class TestMFK(SMTestCase):
         yt_e = hf_function(xt_e)
         yt_c = lf_function(xt_c)
 
-        sm = MFK(theta0=np.array(xt_e.shape[1] * [1.0]))
+        sm = MFK(theta0=xt_e.shape[1] * [1.0])
 
         # low-fidelity dataset names being integers from 0 to level-1
         sm.set_training_values(xt_c, yt_c, name=0)
