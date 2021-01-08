cf297a82e1c2a7de29e6c9d6358b2ce7f9a05a2e
anfelopera <anfelopera@utp.edu.co>
2021-01-08 15:19:20 +0100

    writing kpls variations as kpls methods rather than krgBased

--- smt/surrogate_models/kplsk.py
+++ smt/surrogate_models/kplsk.py
@@ -3,27 +3,15 @@ Author: Dr. Mohamed A. Bouhlel <mbouhlel@umich.edu>
 
 This package is distributed under New BSD license.
 """
-import warnings
-import numpy as np
 
-from packaging import version
-from sklearn import __version__ as sklversion
-
-if version.parse(sklversion) < version.parse("0.22"):
-    from sklearn.cross_decomposition.pls_ import PLSRegression as pls
-else:
-    from sklearn.cross_decomposition import PLSRegression as pls
-
-from smt.surrogate_models.krg_based import KrgBased
+from smt.surrogate_models import KPLS
 from smt.utils.kriging_utils import componentwise_distance_PLS, componentwise_distance
 
 
-class KPLSK(KrgBased):
+class KPLSK(KPLS):
     def _initialize(self):
         super(KPLSK, self)._initialize()
         declare = self.options.declare
-        declare("n_comp", 1, types=int, desc="Number of principal components")
-        self.name = "KPLSK"
         # KPLSK used only with "squar_exp" correlations
         declare(
             "corr",
@@ -32,12 +20,7 @@ class KPLSK(KrgBased):
             desc="Correlation function type",
             types=(str),
         )
-
-    def _compute_pls(self, X, y):
-        _pls = pls(self.options["n_comp"])
-        self.coeff_pls = _pls.fit(X.copy(), y.copy()).x_rotations_
-
-        return X, y
+        self.name = "KPLSK"
 
     def _componentwise_distance(self, dx, opt=0):
         if opt == 0:
