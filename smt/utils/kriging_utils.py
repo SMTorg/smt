14f643a6872583f9c4c079d2f6dbbc9fe6ce8f83
raul-rufato <61620604+raul-rufato@users.noreply.github.com>
2021-01-31 14:34:40 +0100

    Update kriging_utils.py

--- smt/utils/kriging_utils.py
+++ smt/utils/kriging_utils.py
@@ -87,7 +87,7 @@ def standardization(X, y, scale_X_to_unit=False):
 def cross_distances(X):
 
     """
-    Computes the nonzero componentwise cross-distances between the vectors
+    Computes the nonzero componentwise cross-distances between the vectors 
     in X.
 
     Parameters
@@ -121,6 +121,274 @@ def cross_distances(X):
         D[ll_0:ll_1] = X[k] - X[(k + 1) : n_samples]
 
     return D, ij.astype(np.int)
+
+def gower_distances(X,y=None):
+    """
+    Some parts were extracted from gower 0.0.5 library
+    Computes the nonzero Gower-distances between the vectors
+    in X.
+    Parameters
+    ----------
+    X: np.ndarray [n_obs, dim]
+            - The input variables.
+    Returns
+    -------
+    D: np.ndarray [n_obs * (n_obs - 1) / 2, dim]
+            - The gower distances between the vectors in X.
+    ij: np.ndarray [n_obs * (n_obs - 1) / 2, 2]
+            - The indices i and j of the vectors in X associated to the cross-
+              distances in D.
+    """
+    # function checks
+    if y is None: Y = X 
+    else: Y = y
+    if not isinstance(X, np.ndarray): 
+        if not np.array_equal(X.columns, Y.columns): raise TypeError("X and Y must have same columns!")   
+    else: 
+         if not X.shape[1] == Y.shape[1]: raise TypeError("X and Y must have same y-dim!")  
+
+    if issparse(X) or issparse(Y): raise TypeError("Sparse matrices are not supported!")        
+
+    x_n_rows, x_n_cols = X.shape
+    y_n_rows, y_n_cols = Y.shape 
+
+    if not isinstance(X, np.ndarray): 
+        is_number = np.vectorize(lambda x: not np.issubdtype(x, np.number))
+        cat_features = is_number(X.dtypes)    
+    else:
+        cat_features = np.zeros(x_n_cols, dtype=bool)
+        for col in range(x_n_cols):
+            if not np.issubdtype(type(X[0, col]), np.number):
+                cat_features[col]=True
+
+    # print(cat_features)
+
+    if not isinstance(X, np.ndarray): X = np.asarray(X)
+    if not isinstance(Y, np.ndarray): Y = np.asarray(Y)
+
+    Z = np.concatenate((X,Y))
+
+    x_index = range(0,x_n_rows)
+    y_index = range(x_n_rows,x_n_rows+y_n_rows)
+
+    Z_num = Z[:,np.logical_not(cat_features)]
+
+    num_cols = Z_num.shape[1]
+    num_ranges = np.zeros(num_cols)
+    num_max = np.zeros(num_cols)
+
+    for col in range(num_cols):
+        col_array = Z_num[:, col].astype(np.float32) 
+        max = np.nanmax(col_array)
+        min = np.nanmin(col_array)
+
+        if np.isnan(max):
+            max = 0.0
+        if np.isnan(min):
+            min = 0.0
+        num_max[col] = max
+        num_ranges[col] = (1 - min / max) if (max != 0) else 0.0
+
+    # This is to normalize the numeric values between 0 and 1.
+    Z_num = np.divide(Z_num ,num_max,out=np.zeros_like(Z_num), where=num_max!=0)
+    Z_cat = Z[:,cat_features]
+
+
+
+    X_cat = Z_cat[x_index,]
+    X_num = Z_num[x_index,]
+    Y_cat = Z_cat[y_index,]
+    Y_num = Z_num[y_index,]
+
+
+    n_samples, n_features = X_num.shape
+    n_nonzero_cross_dist = n_samples * (n_samples - 1) // 2
+    ij = np.zeros((n_nonzero_cross_dist, 2), dtype=np.int)
+    D_num = np.zeros((n_nonzero_cross_dist, n_features))
+    ll_1 = 0
+
+    for k in range(n_samples - 1):
+        ll_0 = ll_1
+        ll_1 = ll_0 + n_samples - k - 1
+        ij[ll_0:ll_1, 0] = k
+        ij[ll_0:ll_1, 1] = np.arange(k + 1, n_samples)
+        abs_delta = np.abs(X_num[k] - Y_num[(k + 1) : n_samples])
+        D_num[ll_0:ll_1] = np.divide(abs_delta, num_ranges, 
+                                     out=np.zeros_like(abs_delta),
+                                     where=num_ranges!=0)
+
+
+    n_samples, n_features = X_cat.shape
+    n_nonzero_cross_dist = n_samples * (n_samples - 1) // 2
+    D_cat = np.zeros((n_nonzero_cross_dist, n_features))
+    ll_1 = 0
+
+    for k in range(n_samples - 1):
+        ll_0 = ll_1
+        ll_1 = ll_0 + n_samples - k - 1
+        D_cat[ll_0:ll_1] = np.where(X_cat[k] == Y_cat[(k + 1) : n_samples],
+                                    np.zeros_like(X_cat[k]),
+                                    np.ones_like(X_cat[k]))
+
+    D = np.concatenate((D_cat,D_num), axis=1)
+
+    return D, ij.astype(np.int)
+
+
+
+def gower_corr(theta, d):
+
+    """
+    Gower autocorrelation model.
+    Parameters
+    ----------
+    theta : list[ncomp]
+        the autocorrelation parameter(s).
+    d: np.ndarray[n_obs * (n_obs - 1) / 2, n_comp]
+        |d_i * coeff_pls_i| if PLS is used, |d_i| otherwise
+    Returns
+    -------
+    r : np.ndarray[n_obs * (n_obs - 1) / 2,1]
+        An array containing the values of the autocorrelation model.
+    """
+
+    r = np.zeros((d.shape[0], 1))
+    n_components = d.shape[1]
+
+    # Construct/split the correlation matrix
+    i, nb_limit = 0, int(1e4)
+    while True:
+        if i * nb_limit > d.shape[0]:
+            return r
+        else:
+            r[i * nb_limit : (i + 1) * nb_limit, 0] = np.exp(
+                np.divide(-np.sum(
+                    theta.reshape(1, n_components)
+                    * d[i * nb_limit : (i + 1) * nb_limit, :],
+                    axis=1,
+                ),theta.sum())
+            )
+            i += 1
+
+def gower_matrix(data_x, data_y=None, weight=None, cat_features=None):  
+    "this function was copied from gower 0.0.5 code"
+    # function checks
+    X = data_x
+    if data_y is None: Y = data_x 
+    else: Y = data_y 
+    if not isinstance(X, np.ndarray): 
+        if not np.array_equal(X.columns, Y.columns): raise TypeError("X and Y must have same columns!")   
+    else: 
+         if not X.shape[1] == Y.shape[1]: raise TypeError("X and Y must have same y-dim!")  
+
+    if issparse(X) or issparse(Y): raise TypeError("Sparse matrices are not supported!")        
+
+    x_n_rows, x_n_cols = X.shape
+    y_n_rows, y_n_cols = Y.shape 
+
+    if cat_features is None:
+        if not isinstance(X, np.ndarray): 
+            is_number = np.vectorize(lambda x: not np.issubdtype(x, np.number))
+            cat_features = is_number(X.dtypes)    
+        else:
+            cat_features = np.zeros(x_n_cols, dtype=bool)
+            for col in range(x_n_cols):
+                if not np.issubdtype(type(X[0, col]), np.number):
+                    cat_features[col]=True
+    else:          
+        cat_features = np.array(cat_features)
+
+    # print(cat_features)
+
+    if not isinstance(X, np.ndarray): X = np.asarray(X)
+    if not isinstance(Y, np.ndarray): Y = np.asarray(Y)
+
+    Z = np.concatenate((X,Y))
+
+    x_index = range(0,x_n_rows)
+    y_index = range(x_n_rows,x_n_rows+y_n_rows)
+
+    Z_num = Z[:,np.logical_not(cat_features)]
+
+    num_cols = Z_num.shape[1]
+    num_ranges = np.zeros(num_cols)
+    num_max = np.zeros(num_cols)
+
+    for col in range(num_cols):
+        col_array = Z_num[:, col].astype(np.float32) 
+        max = np.nanmax(col_array)
+        min = np.nanmin(col_array)
+
+        if np.isnan(max):
+            max = 0.0
+        if np.isnan(min):
+            min = 0.0
+        num_max[col] = max
+        num_ranges[col] = (1 - min / max) if (max != 0) else 0.0
+
+    # This is to normalize the numeric values between 0 and 1.
+    Z_num = np.divide(Z_num ,num_max,out=np.zeros_like(Z_num), where=num_max!=0)
+    Z_cat = Z[:,cat_features]
+
+    if weight is None:
+        weight = np.ones(Z.shape[1])
+
+    #print(weight)    
+
+    weight_cat=weight[cat_features]
+    weight_num=weight[np.logical_not(cat_features)]   
+
+    out = np.zeros((x_n_rows, y_n_rows), dtype=np.float32)
+
+    weight_sum = weight.sum()
+
+    X_cat = Z_cat[x_index,]
+    X_num = Z_num[x_index,]
+    Y_cat = Z_cat[y_index,]
+    Y_num = Z_num[y_index,]
+
+   # print(X_cat,X_num,Y_cat,Y_num)
+
+    for i in range(x_n_rows):          
+        j_start= i        
+        if x_n_rows != y_n_rows:
+            j_start = 0
+        # call the main function
+        res = gower_get(X_cat[i,:], 
+                          X_num[i,:],
+                          Y_cat[j_start:y_n_rows,:],
+                          Y_num[j_start:y_n_rows,:],
+                          weight_cat,
+                          weight_num,
+                          weight_sum,
+                          cat_features,
+                          num_ranges,
+                          num_max) 
+        #print(res)
+        out[i,j_start:]=res
+        if x_n_rows == y_n_rows: out[i:,j_start]=res
+
+    return out
+
+
+def gower_get(xi_cat,xi_num,xj_cat,xj_num,feature_weight_cat,
+              feature_weight_num,feature_weight_sum,categorical_features,
+              ranges_of_numeric,max_of_numeric ):
+    "this function was copied from gower 0.0.5 code"
+    # categorical columns
+    sij_cat = np.where(xi_cat == xj_cat,np.zeros_like(xi_cat),np.ones_like(xi_cat))
+    sum_cat = np.multiply(feature_weight_cat,sij_cat).sum(axis=1) 
+
+    # numerical columns
+    abs_delta=np.absolute(xi_num-xj_num)
+    sij_num=np.divide(abs_delta, ranges_of_numeric, out=np.zeros_like(abs_delta), where=ranges_of_numeric!=0)
+
+    sum_num = np.multiply(feature_weight_num,sij_num).sum(axis=1)
+    sums= np.add(sum_cat,sum_num)
+    sum_sij = np.divide(sums,feature_weight_sum)
+
+    return sum_sij
+
 
 
 def differences(X, Y):
