3ba7aaaeadf8a3c5086f2f293bf3bd4c4aca7c08
Andres F. López-Lopera <anfelopera@users.noreply.github.com>
2021-04-16 18:49:21 +0200

    Fix krg_based to allow n_start=1
    * Tutorial SMT with noise
    
    * adding a label for training data
    
    * correcting typos + minor details
    
    * adding random_state in nestedLHS
    
    * fixing a random seed in MFK tests
    
    * allowing n_start=1
    
    * adding krg_test with n_start=1
    
    * adding another test with n_start=1

--- smt/surrogate_models/krg_based.py
+++ smt/surrogate_models/krg_based.py
@@ -1103,11 +1103,15 @@ class KrgBased(SurrogateModel):
                 theta_limits = np.repeat(
                     np.log10([theta_bounds]), repeats=len(theta0), axis=0
                 )
-                sampling = LHS(
-                    xlimits=theta_limits, criterion="maximin", random_state=41
-                )
-                theta_lhs_loops = sampling(self.options["n_start"])
-                theta_all_loops = np.vstack((theta0, theta0_rand, theta_lhs_loops))
+                theta_all_loops = np.vstack((theta0, theta0_rand))
+                
+                if self.options["n_start"] > 1:
+                    sampling = LHS(
+                        xlimits=theta_limits, criterion="maximin", random_state=41
+                    )
+                    theta_lhs_loops = sampling(self.options["n_start"])
+                    theta_all_loops = np.vstack((theta_all_loops, theta_lhs_loops))
+                    
                 optimal_theta_res = {"fun": float("inf")}
                 try:
                     if self.options["hyper_opt"] == "Cobyla":
