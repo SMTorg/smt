c356804e353db6a1758dbc89a1b199b41b35bacf
Paul-Saves <paul.saves@alumni.enac.fr>
2021-08-23 10:36:52 +0200

    fix test gower (documentation remains to be built)

--- smt/applications/tests/test_mixed_integer.py
+++ smt/applications/tests/test_mixed_integer.py
@@ -397,13 +397,13 @@ class TestMixedInteger(unittest.TestCase):
         from smt.surrogate_models import KRG
         import matplotlib.pyplot as plt
         import numpy as np
-
-        xt = np.linspace(1.0, 5.0, 5)
+        
+        xt = np.array([1.0,3.0, 5.0])
         x_train = np.array(["%.2f" % i for i in xt], dtype=object)
-        yt = np.array([0.0, 1.0, 1.5, 0.5, 1.0])
-
+        yt = np.array([0.0, 1.0, 1.5])
+        
         xlimits = [["0.0", "1.0", " 2.0", "3.0", "4.0"]]
-
+        
         # Surrogate
         sm = MixedIntegerSurrogateModel(
             use_gower_distance=True,
@@ -413,19 +413,21 @@ class TestMixedInteger(unittest.TestCase):
         )
         sm.set_training_values(x_train, yt)
         sm.train()
-
+        
         # DOE for validation
-        num = 101
+        num = 6
         x = np.linspace(0, 5, num)
         x_pred = np.array(["%.2f" % i for i in x], dtype=object)
         y = sm.predict_values(x_pred)
-
-        plt.plot(xt, yt, "o")
-        plt.plot(x, y)
-        plt.xlabel("actual")
-        plt.ylabel("prediction")
+        
+        plt.plot(xt, yt, "o",label='data')
+        plt.plot(x, y, "d",color="red",markersize=3,label='pred')
+        plt.xlabel("x")
+        plt.ylabel("y")
+        plt.legend()
         plt.show()
-
+        
+        
 
 if __name__ == "__main__":
     TestMixedInteger().run_mixed_integer_context_example()
