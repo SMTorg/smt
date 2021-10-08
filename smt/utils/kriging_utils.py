480ce2f474507d2f57cdb0fc848be1c1e429af27
Paul-Saves <paul.saves@alumni.enac.fr>
2021-10-08 11:04:25 +0200

    end of spd

--- smt/utils/kriging_utils.py
+++ smt/utils/kriging_utils.py
@@ -436,8 +436,8 @@ def matrix_data_corr(corr, theta, d, Lij, nlevels, cat_features):
     ##Theta_cat_i loop
     i = 0
     ###
-    theta_cat = theta[np.logical_not(theta_cont_features)] 
-    theta_cat[: -nlevels[i]] =theta_cat[: -nlevels[i]]*(np.pi/20)
+    theta_cat = theta[np.logical_not(theta_cont_features)]
+    theta_cat[: -nlevels[i]] = theta_cat[: -nlevels[i]] * (np.pi / 20)
     d_cat = d[:, cat_features]
 
     _, cat_i_ij_full = cross_distances(np.zeros((nlevels[i], nlevels[i])))
@@ -453,8 +453,8 @@ def matrix_data_corr(corr, theta, d, Lij, nlevels, cat_features):
             if j == k + j:
                 Theta_mat[j, k + j] = 1
             else:
-                Theta_mat[j, k + j] = theta_cat[v]
-                Theta_mat[k + j, j] = theta_cat[v]
+                Theta_mat[j, k + j] = 1 - theta_cat[v]
+                Theta_mat[k + j, j] = 1 - theta_cat[v]
                 v = v + 1
 
     for j in range(nlevels[i]):
@@ -475,26 +475,19 @@ def matrix_data_corr(corr, theta, d, Lij, nlevels, cat_features):
                     L[k + j, j] = np.cos(Theta_mat[k + j, j])
                     for l in range(j):
                         L[k + j, j] = L[k + j, j] * np.sin(Theta_mat[k + j, l])
-
-    #   T2=np.exp(-Theta_mat)
-    # T2=
-    
-    T2 = np.dot(L, L.T) + np.eye(nlevels[i]) *  (1e-10)
-    C = np.linalg.cholesky(T2)
-
-    T2 = np.exp(-2)*np.exp(2*T2)
-
-    C = np.linalg.cholesky(T2)
-    
-    
+    T2 = np.dot(L, L.T)
+    T2 = (T2 - 1) * 20
+    T2 = np.exp(T2) + np.eye(nlevels[i]) * (1e-11)
     for k in range(np.shape(Lij[i])[0]):
         indi = int(Lij[i][k][0])
         indj = int(Lij[i][k][1])
         if indi == indj:
             r_cat[k, 0] = 1.0
-        else: 
-            r_cat[k, 0] = np.exp(-theta_cat[int(int(nlevels[i] * (nlevels[i] - 1) / 2) + indi)] -  theta_cat[int(int(nlevels[i] * (nlevels[i] - 1) / 2) + indj)]) *( T2[indi, indj])
-
+        else:
+            r_cat[k, 0] = np.exp(
+                -theta_cat[int(int(nlevels[i] * (nlevels[i] - 1) / 2) + indi)]
+                - theta_cat[int(int(nlevels[i] * (nlevels[i] - 1) / 2) + indj)]
+            ) * (T2[indi, indj])
     r = np.multiply(r_cont, r_cat)
     return r
 
