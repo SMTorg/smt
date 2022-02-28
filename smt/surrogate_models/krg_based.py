cd45452cb3b02eca0629ec10a67e22911c838f46
Paul-Saves <paul.saves@alumni.enac.fr>
2022-02-28 20:16:01 +0100

    20?

--- smt/surrogate_models/krg_based.py
+++ smt/surrogate_models/krg_based.py
@@ -1098,7 +1098,7 @@ class KrgBased(SurrogateModel):
                 )
                 return res
 
-        limit, _rhobeg = 50 * len(self.options["theta0"]), 0.5
+        limit, _rhobeg = 20 * len(self.options["theta0"]), 0.5
         exit_function = False
         if "KPLSK" in self.name:
             n_iter = 1
