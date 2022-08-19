Fix wing weight formula (#381)
Co-authored-by: Paul-Saves <paul.saves@alumni.enac.fr>

smt/problems/wing_weight.py
@@ -64,7 +64,7 @@ class WingWeight(Problem):
                 0.036
                 * x0 ** 0.758
                 * x1 ** 0.0035
-                * (x2 / np.cos(deg2rad(x3)) ** 2)
+                * (x2 / np.cos(deg2rad(x3)) ** 2)**(0.6)
                 * x4 ** 0.006
                 * x5 ** 0.04
                 * (100 * x6 / np.cos(deg2rad(x3))) ** (-0.3)