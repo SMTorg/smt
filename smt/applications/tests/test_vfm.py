fix gradient algorithm

smt/applications/tests/test_vfm.py
@@ -68,7 +68,12 @@ class TestVFM(SMTestCase):
         Bridge_candidate = "KRG"
         type_bridge = "Multiplicative"
         optionsLF = {}
-        optionsB = {"theta0": [1e-2] * ndim, "print_prediction": False, "deriv": False}
+        optionsB = {
+            "theta0": [1e-2] * ndim,
+            "print_prediction": False,
+            "deriv": False,
+            "hyper_opt": "Cobyla",
+        }
 
         # Construct low/high fidelity data and validation points
         sampling = LHS(xlimits=funLF.xlimits, criterion="m", random_state=42)
@@ -138,7 +143,12 @@ class TestVFM(SMTestCase):
         Bridge_candidate = "KRG"
         type_bridge = "Multiplicative"
         optionsLF = {}
-        optionsB = {"theta0": [1e-2] * ndim, "print_prediction": False, "deriv": False}
+        optionsB = {
+            "theta0": [1e-2] * ndim,
+            "print_prediction": False,
+            "deriv": False,
+            "hyper_opt": "Cobyla",
+        }
 
         # Construct low/high fidelity data and validation points
         sampling = LHS(xlimits=funLF.xlimits, criterion="m")
@@ -200,7 +210,9 @@ class TestVFM(SMTestCase):
             yp = M.predict_values(np.atleast_2d(xt[0]))
             dyp = M.predict_derivatives(np.atleast_2d(xt[0]), kx=0)
         self.assert_error(yp, np.array([[0.015368, 0.367424]]), atol=2e-2, rtol=3e-2)
-        self.assert_error(dyp, np.array([[0.07007729, 3.619421]]), atol=3e-1, rtol=1e-2)
+        self.assert_error(
+            dyp, np.array([[-3.11718627e-03, 3.19506239e00]]), atol=3e-1, rtol=1e-2
+        )
 
     def test_QP_KRG_additive(self):
         with Silence():
@@ -214,7 +226,7 @@ class TestVFM(SMTestCase):
 
         self.assert_error(yp, np.array([[0.015368, 0.367424]]), atol=1e-2, rtol=1e-2)
         self.assert_error(
-            dyp, np.array([[1.16130832e-03, 4.36712162e00]]), atol=3e-1, rtol=1e-2
+            dyp, np.array([[0.02596425, 4.70243162]]), atol=3e-1, rtol=1e-2
         )
 
     def test_KRG_KRG_mult(self):
@@ -228,7 +240,9 @@ class TestVFM(SMTestCase):
             dyp = M.predict_derivatives(np.atleast_2d(xt[0]), kx=0)
 
         self.assert_error(yp, np.array([[0.015368, 0.367424]]), atol=2e-2, rtol=3e-2)
-        self.assert_error(dyp, np.array([[0.07007729, 3.619421]]), atol=3e-1, rtol=1e-2)
+        self.assert_error(
+            dyp, np.array([[-3.11718627e-03, 3.19506239e00]]), atol=3e-1, rtol=1e-2
+        )
 
     def test_QP_KRG_mult(self):
         with Silence():

smt/surrogate_models/krg_based.py
@@ -1016,7 +1016,7 @@ class KrgBased(SurrogateModel):
         gamma = par["gamma"]
         Q = par["Q"]
         G = par["G"]
-        sigma_2 = par["sigma2"]
+        sigma_2 = par["sigma2"] + self.options["nugget"]
 
         nb_theta = len(theta)
         grad_red = np.zeros(nb_theta)

smt/surrogate_models/tests/test_krg_het_noise.py
@@ -23,7 +23,13 @@ class Test(SMTestCase):
         xt_full = np.array(3 * xt.tolist())
         yt_full = np.concatenate((yt, yt + 0.2 * yt_std_rand, yt - 0.2 * yt_std_rand))
 
-        sm = KRG(theta0=[1.0], eval_noise=True, use_het_noise=True, n_start=1)
+        sm = KRG(
+            theta0=[1.0],
+            eval_noise=True,
+            use_het_noise=True,
+            n_start=1,
+            hyper_opt="Cobyla",
+        )
         sm.set_training_values(xt_full, yt_full)
         sm.train()
 