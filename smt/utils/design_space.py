d5e297442ddaad021e03298acdcd1a07a0689443
Paul-Saves <paul.saves@alumni.enac.fr>
2024-02-06 17:27:59 +0100

    toseed

--- smt/utils/design_space.py
+++ smt/utils/design_space.py
@@ -745,11 +745,7 @@ class DesignSpace(BaseDesignSpace):
                     cs_vars[name] = CategoricalHyperparameter(name, choices=dv.values)
                 else:
                     raise ValueError(f"Unknown variable type: {dv!r}")
-            seed = None
-            if isinstance(random_state, int):
-                seed = random_state
-            elif isinstance(random_state, np.random.RandomState):
-                seed = random_state.get_state()[1][0]
+            seed = self._to_seed(random_state)
 
             self._cs = NoDefaultConfigurationSpace(space=cs_vars, seed=seed)
 
