103557b70a5f8ee9c7ece82bef0b78517addb6fa
Shisohka <reim.hugo@gmail.com>
2024-03-28 11:28:18 +0100

    new file for pod+gp

---
+++ smt/surrogate_models/pod_gp.py
@@ -0,0 +1,34 @@
+"""
+Author: Dr. Mohamed A. Bouhlel <mbouhlel@umich.edu>
+
+This package is distributed under New BSD license.
+"""
+
+from smt.surrogate_models.krg_based import KrgBased
+from smt.utils.kriging import componentwise_distance
+
+
+class POD_GP(KrgBased):
+    name = "POD+GP"
+
+    def _initialize(self):
+        super()._initialize()
+        declare = self.options.declare
+        declare(
+            "corr",
+            "squar_exp",
+            values=("pow_exp", "abs_exp", "squar_exp", "matern52", "matern32"),
+            desc="Correlation function type",
+            types=(str),
+        )
+
+    def _componentwise_distance(self, dx, opt=0, theta=None, return_derivative=False):
+        d = componentwise_distance(
+            dx,
+            self.options["corr"],
+            self.nx,
+            self.options["pow_exp_power"],
+            theta=theta,
+            return_derivative=return_derivative,
+        )
+        return d
