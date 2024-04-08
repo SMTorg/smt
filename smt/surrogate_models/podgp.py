24c928eda03af1c7d0f84e3d4d0ad9c48ad116a8
Shisohka <reim.hugo@gmail.com>
2024-04-08 17:32:39 +0200

    developping pod function and heritage with SurrogateModel class

--- smt/surrogate_models/podgp.py
+++ smt/surrogate_models/podgp.py
@@ -4,31 +4,78 @@ Author: Dr. Mohamed A. Bouhlel <mbouhlel@umich.edu>
 This package is distributed under New BSD license.
 """
 
-from smt.surrogate_models.krg_based import KrgBased
+from typing import Optional
+from smt.surrogate_models.surrogate_model import SurrogateModel
+from smt.surrogate_models import KRG
 from smt.utils.kriging import componentwise_distance
+from sklearn.decomposition import PCA
+import numpy as np
+import warnings
 
 
-class PODGP(KrgBased):
+class PODGP(SurrogateModel):
     name = "POD+GP"
 
     def _initialize(self):
         super()._initialize()
-        declare = self.options.declare
-        declare(
-            "corr",
-            "squar_exp",
-            values=("pow_exp", "abs_exp", "squar_exp", "matern52", "matern32"),
-            desc="Correlation function type",
-            types=(str),
-        )
+        self.basis = np.array([])
+        self.coeff = np.array([])
+        self.sm_list = []
+        self.KRG_options = []
+        self.x_train = np.array([])
+        self.database = np.array([])
+        self.n_mods = 0
+        self.tol = 0
+        self.svd = PCA(svd_solver = 'randomized')
+        # self.azerty = kwargs
+        
+        # print(self.azerty.items())
+        
+    def _predict_derivatives(x, kx):
+        return None
+        
+    def _predict_output_derivatives(x):
+        return None
+        
+    def _predict_values(x):
+        return None
+    
+    def _predict_variance_derivatives(x, kx):
+        return None	
+    
+    def _predict_variances(x):
+        return None
+    
+    def _set_training_derivatives(self, xt: np.ndarray, dyt_dxt: np.ndarray, kx: int, name: Optional[str] = None):
+        return None
+    
+    def _set_training_values(self, xt: np.ndarray, yt: np.ndarray, name=None) -> None:
+        return None
+    
+    def _train():
+        return None
 
-    def _componentwise_distance(self, dx, opt=0, theta=None, return_derivative=False):
-        d = componentwise_distance(
-            dx,
-            self.options["corr"],
-            self.nx,
-            self.options["pow_exp_power"],
-            theta=theta,
-            return_derivative=return_derivative,
-        )
-        return d
+    def POD(self, **kwargs):
+        dico = kwargs
+        choice_svd = None
+        self.database = kwargs['database']
+        if "n_mods" in dico.keys():
+            self.n_mods = kwargs["n_mods"]
+            choic_svd = "mod"
+        if "tol_svd" in dico.keys():
+            if choice_svd != None:
+                raise ValueError(
+                    "svd can't use both arguments n_mods and tol_svd at the same time."
+                )
+            self.tol_svd = kwargs["tol_svd"]
+            choic_svd = "mod"
+        
+            
+        self.svd.fit(self.database.T)
+        
+        U = self.svd.components_.T
+        S = self.svd.singular_values_
+        
+        
+    def GP(self, **kwargs):
+        dico = kwargs
