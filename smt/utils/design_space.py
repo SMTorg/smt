9a54c6cacf91754aad12cfdf2e22d1a80c01f753
Paul-Saves <paul.saves@alumni.enac.fr>
2024-04-10 17:36:19 +0200

    designspace

--- smt/utils/design_space.py
+++ smt/utils/design_space.py
@@ -922,6 +922,8 @@ class DesignSpace(BaseDesignSpace):
                 else:
                     constraint_clause = ForbiddenLessThanRelation(param2, param1)
                     self._cs.add_forbidden_clause(constraint_clause)
+            else :
+                raise ValueError("Bad definition of DesignSpace.")
 
         ## Fix to make constraints work correctly with either IntegerVariable or OrdinalVariable
         ## ConfigSpace is malfunctioning
