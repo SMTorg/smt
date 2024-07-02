3e5dbfdf8f96e5b1958f99b7d4162ac78e71b3c8
MaelTremouille <tremat82@gmail.com>
2024-07-02 17:23:05 +0200

    issue Chlosky decomposition of R_bis

--- smt/surrogate_models/krg_based.py
+++ smt/surrogate_models/krg_based.py
@@ -982,27 +982,31 @@ class KrgBased(SurrogateModel):
         R_noisy = np.eye(self.nt) * (1.0 + nugget + noise)
         R_noisy[self.ij[:, 0], self.ij[:, 1]] = r[:, 0]
         R_noisy[self.ij[:, 1], self.ij[:, 0]] = r[:, 0]
-        R = np.eye(self.nt) * (1.0 + nugget)
-        R[self.ij[:, 0], self.ij[:, 1]] = r[:, 0]
-        R[self.ij[:, 1], self.ij[:, 0]] = r[:, 0]
+        R_bis = np.eye(self.nt) * (1.0 + nugget)
+        R_bis[self.ij[:, 0], self.ij[:, 1]] = r[:, 0]
+        R_bis[self.ij[:, 1], self.ij[:, 0]] = r[:, 0]
 
         p = 0
         q = 0
 
         # Cholesky decomposition of R and computation of its inverse
-        C = linalg.cholesky(R, lower=True)
-        C_inv = np.linalg.inv(C)
-        R_inv = np.dot(C_inv.T, C_inv)
+        try:
+            C_bis = linalg.cholesky(R_bis, lower=True)
+            # C_bis_inv = np.linalg.inv(C_bis)
+            # R_bis_inv = np.dot(C_bis_inv.T, C_bis_inv)
+        except ValueError as e:
+            print(R_bis)
+            print(e)
 
-        R_ri = R_noisy @ R_inv @ R_noisy
+        # R_ri = R_noisy @ R_bis_inv @ R_noisy
         reduced_likelihood_function_value, par, sigma2 = self._compute_sigma2(
             R_noisy, reduced_likelihood_function_value, par, p, q, is_ri=False
         )
-        reduced_likelihood_function_value_ri, par, sigma2_ri = self._compute_sigma2(
-            R_ri, reduced_likelihood_function_value, par, p, q, is_ri=True
-        )
+        # reduced_likelihood_function_value_ri, par, sigma2_ri = self._compute_sigma2(
+        #     R_ri, reduced_likelihood_function_value, par, p, q, is_ri=True
+        # )
         par["sigma2"] = sigma2 * self.y_std**2.0
-        par["sigma2_ri"] = sigma2_ri * self.y_std**2.0
+        # par["sigma2_ri"] = sigma2_ri * self.y_std**2.0
 
         if self.name in ["MGP"]:
             reduced_likelihood_function_value += self._reduced_log_prior(theta)
@@ -1675,7 +1679,7 @@ class KrgBased(SurrogateModel):
             A = self.optimal_par["sigma2_ri"]
         else:
             A = self.optimal_par["sigma2"]
-        print(self.optimal_par["sigma2"], self.optimal_par["sigma2_ri"])
+        print(A)
         A = self.optimal_par["sigma2"]
         B = 1.0 - (rt**2.0).sum(axis=0) + (u**2.0).sum(axis=0)
         # machine precision: force to zero!
