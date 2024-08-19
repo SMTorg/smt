e4fd6a6f5be58ce8c0a87ecadb5ac02c56b37d24
Paul-Saves <paul.saves@alumni.enac.fr>
2024-08-19 11:17:29 +0200

    test

--- smt/utils/design_space.py
+++ smt/utils/design_space.py
@@ -1309,21 +1309,28 @@ class ArchDesignSpaceGraph(DesignSpace):
         self._is_decreed = design_space._is_decreed
 
     def _sample_valid_x(
-        self, n: int, random_state=None
+        self, n: int, random_state=None, return_render=False,
     ) -> Tuple[np.ndarray, np.ndarray]:
         """Sample design vectors"""
         # Get design vectors and get the is_active matrix
+        configs0 = []
         configs1 = []
         configs2 = []
         for i in range(n):
+            configs0.append(
+                self.graph_proc.get_graph(self.graph_proc.get_random_design_vector())[0]
+            )
             configs1.append(
                 self.graph_proc.get_graph(self.graph_proc.get_random_design_vector())[1]
             )
             configs2.append(
                 self.graph_proc.get_graph(self.graph_proc.get_random_design_vector())[2]
             )
-        return np.array(configs1), np.array(configs2)
-
+        if return_render :
+            return np.array(configs1), np.array(configs2), np.array(configs0)
+        else :
+            return np.array(configs1), np.array(configs2)
+        
     def _is_conditionally_acting(self) -> np.ndarray:
         # Decreed variables are the conditionally acting variables
         return np.array(self.graph_proc.dv_is_conditionally_active)
