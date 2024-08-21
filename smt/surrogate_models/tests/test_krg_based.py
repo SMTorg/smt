0995844e935d8f7cf706f4a36e3db00e6fa34b8f
Paul-Saves <paul.saves@alumni.enac.fr>
2024-08-21 14:01:47 +0200

    add sgp for non-normalized data

--- smt/surrogate_models/tests/test_krg_based.py
+++ smt/surrogate_models/tests/test_krg_based.py
@@ -483,16 +483,6 @@ class TestKrgBased(unittest.TestCase):
                 [17.06],
             ]
         )
-
-        sm = SGP(n_start=25, hyper_opt="Cobyla", random_state=np.random.RandomState(0))
-        sm.set_training_values(xt, yt)
-        sm.train()
-        sm.predict_values(x)  # predictive mean
-        sm.predict_variances(x)  # predictive variance
-        sm = KRG(n_start=25, hyper_opt="Cobyla", random_state=np.random.RandomState(0))
-        sm.set_training_values(xt, yt)
-        sm.train()
-        # predictions
         x = np.array(
             [
                 [1.9e3, 2.5e-1, 8.00000000e00, 3.7e-01],
@@ -501,6 +491,16 @@ class TestKrgBased(unittest.TestCase):
                 [4.6e03, 2.1e-01, -1.00000000e00, 8.2e-01],
             ]
         )
+        sm = SGP(n_start=25, hyper_opt="Cobyla", random_state=np.random.RandomState(0))
+        sm.set_training_values(xt, yt)
+        sm.train()
+        # predictions
+        sm.predict_values(x)  # predictive mean
+        sm.predict_variances(x)  # predictive variance
+        sm = KRG(n_start=25, hyper_opt="Cobyla", random_state=np.random.RandomState(0))
+        sm.set_training_values(xt, yt)
+        sm.train()
+        # predictions
         sm.predict_values(x)  # predictive mean
         sm.predict_variances(x)  # predictive variance
         sm.predict_derivatives(x, 0)  # predictive variance
