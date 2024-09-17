5a34fd7b5fc84c9eaf5b39ad223f78eabda9b2eb
Paul-Saves <paul.saves@alumni.enac.fr>
2024-09-17 11:04:24 +0200

    update tests

--- smt/applications/tests/test_mixed_integer.py
+++ smt/applications/tests/test_mixed_integer.py
@@ -663,9 +663,8 @@ class TestMixedInteger(unittest.TestCase):
 
         y_sv = sm.predict_variances(Xt)[:, 0]
         var_RMSE = np.linalg.norm(y_sv) / len(Yt)
-        self.assertTrue(pred_RMSE < 1e-7)
-        print("Pred_RMSE", pred_RMSE)
-        self.assertTrue(var_RMSE < 1e-7)
+        self.assertLess(pred_RMSE, 1e-7)
+        self.assertLess(var_RMSE, 1e-7)
         self.assertTrue(
             np.linalg.norm(
                 sm.predict_values(
