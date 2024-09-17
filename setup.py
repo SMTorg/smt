41f80a17ae2f3660b1471b447250cd4f78deaaf6
Paul-Saves <paul.saves@alumni.enac.fr>
2024-09-17 15:24:21 +0200

    fix setup?

--- setup.py
+++ setup.py
@@ -118,6 +118,9 @@ metadata = dict(
         "numba": [  # pip install smt[numba]
             "numba~=0.56.4",
         ],
+        "cs": [  # pip install smt[cs]
+            "ConfigSpace~=0.6.1",
+        ],
         "gpx": ["egobox~=0.20"],  # pip install smt[gpx]
     },
     python_requires=">=3.8",
