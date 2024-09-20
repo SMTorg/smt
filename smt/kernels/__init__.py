108ddf63fd0867d48164cca8e4623896d2778ae3
Nicolas Gonel <gonel.nicolas@sfr.fr>
2024-09-20 10:07:12 +0200

    ruff on __init__ v2

--- smt/kernels/__init__.py
+++ smt/kernels/__init__.py
@@ -1,3 +1,16 @@
+from .kernels import (
+    Kernel,
+    PowExp,
+    SquarSinExp,
+    Matern32,
+    Matern52,
+    ActExp,
+    Constant,
+    Operator,
+    Sum,
+    Product,
+)
+
 __all__ = [
     "Kernel",
     "PowExp",
