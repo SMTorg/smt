d8d0e6ec418ec9733f7c4ca2a31bc87786aeff3e
Paul-Saves <paul.saves@alumni.enac.fr>
2024-09-21 17:55:18 +0200

    try to fix

--- smt/design_space/test/test_design_space_local.py
+++ smt/design_space/test/test_design_space_local.py
@@ -1,14 +1,15 @@
 import unittest
 import os
 
-# Load all tests from test_design_space.py
-loader = unittest.TestLoader()
-suite = loader.discover(start_dir=".", pattern="test_design_space.py")
 os.environ["FORCE_RUN_LOCAL"] = "1"
 
 
 class TestDSLocal(unittest.TestCase):
     def test_design_variables(self):
+        os.environ["FORCE_RUN_LOCAL"] = "1"
+        # Load all tests from test_design_space.py
+        loader = unittest.TestLoader()
+        suite = loader.discover(start_dir=".", pattern="test_design_space.py")
         # Run the tests
         runner = unittest.TextTestRunner()
         runner.run(suite)
