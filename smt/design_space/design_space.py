096e276f43f38c24562580f163b464549bcc01fa
Paul-Saves <paul.saves@alumni.enac.fr>
2024-09-21 17:59:21 +0200

    try to fix

--- smt/design_space/design_space.py
+++ smt/design_space/design_space.py
@@ -7,10 +7,7 @@ This package is distributed under New BSD license.
 import numpy as np
 from smt.sampling_methods.lhs import LHS
 from typing import List, Optional, Sequence, Tuple, Union
-
-from smt.design_space import BaseDesignSpace,HAS_SMTDesignSpace,HAS_CONFIG_SPACE, HAS_ADSG
-
-  
+ 
 
 class Configuration:
     pass
