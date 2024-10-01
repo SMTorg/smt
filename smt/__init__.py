aff6f196f5f75d5c2bfef063af35aea553bd730d
Paul-Saves <paul.saves@alumni.enac.fr>
2024-10-01 09:53:44 +0200

    try for better init

--- smt/__init__.py
+++ smt/__init__.py
@@ -1,1 +1,9 @@
+import glob
+
 __version__ = "2.7.0"
+
+modules = glob.glob(join(dirname(__file__), "*.py"))
+__all__ = [
+    basename(f)[:-3] for f in modules if isfile(f) and not f.endswith("__init__.py")
+]
+repo_root = os.path.abspath(os.path.dirname(__file__))
