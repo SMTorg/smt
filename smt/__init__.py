0b93b8343fdf9afc1955656a654eaff470f582a8
Paul-Saves <paul.saves@alumni.enac.fr>
2024-10-01 10:20:31 +0200

    try for better init

--- smt/__init__.py
+++ smt/__init__.py
@@ -1,16 +1,5 @@
 __version__ = "2.7.0"
 
-import smt.surrogate_models
-import smt.kernels
-import smt.design_space
-import smt.examples
-import smt.sampling_methods
-import smt.applications
-import smt.utils
-import smt.tests
-import smt.src
-import smt.problems
-
 __all__ = [
     "surrogate_models",
     "kernels",
