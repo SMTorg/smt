e15660e021570c3226983bcfc961ce173d492033
relf <remi.lafage@onera.fr>
2024-10-18 12:01:19 +0200

    smt use smt_design_space_ext design space impl if installed
    * other base classes belongs to smt
    * only need HAS_DESIGN_SPACE_EXT

--- smt/design_space/__init__.py
+++ smt/design_space/__init__.py
@@ -1,55 +1,33 @@
-import importlib
+from smt.design_space.design_space import (
+    CategoricalVariable,
+    BaseDesignSpace,
+    FloatVariable,
+    IntegerVariable,
+    OrdinalVariable,
+)
 
-spec = importlib.util.find_spec("smt_design_space")
-if spec:
+try:
+    from smt_design_space_ext import (
+        DesignSpace,
+        ensure_design_space,
+    )
+
     HAS_DESIGN_SPACE_EXT = True
-    HAS_CONFIG_SPACE = True
-    HAS_ADSG = True
-else:
+except ImportError:
+    from .design_space import (
+        DesignSpace,
+        ensure_design_space,
+    )
+
     HAS_DESIGN_SPACE_EXT = False
-    HAS_CONFIG_SPACE = False
-    HAS_ADSG = False
-
-
-if HAS_DESIGN_SPACE_EXT:
-    from smt_design_space.design_space import (
-        CategoricalVariable,
-        DesignSpace,
-        BaseDesignSpace,
-        FloatVariable,
-        IntegerVariable,
-        OrdinalVariable,
-        ensure_design_space,
-    )
-
-else:
-    from smt.design_space.design_space import (
-        CategoricalVariable,
-        DesignSpace,
-        FloatVariable,
-        IntegerVariable,
-        OrdinalVariable,
-        ensure_design_space,
-        BaseDesignSpace,
-    )
-
-if HAS_DESIGN_SPACE_EXT:
-    from smt_design_space.design_space import DesignSpaceGraph
-else:
-
-    class DesignSpaceGraph:
-        pass
-
 
 __all__ = [
-    "HAS_DESIGN_SPACE_EXT",
-    "HAS_CONFIG_SPACE",
-    "HAS_ADSG",
     "BaseDesignSpace",
-    "DesignSpace",
     "FloatVariable",
     "IntegerVariable",
     "OrdinalVariable",
     "CategoricalVariable",
+    "DesignSpace",
     "ensure_design_space",
+    "HAS_DESIGN_SPACE_EXT",
 ]
