61bf8c1731eb43fef65a44fa6976d58ca6d3111c
Enrico Stragiotti <44843509+enricostragiotti@users.noreply.github.com>
2024-12-17 10:58:46 +0100

    Add the option to specify a seed for the Random sampling method (#696)
    * Add the option to specify a seed for the Random sampling method
    
    * Behaviour is controlled by the numpy version used
    
    * Create unit tests
    
    * Update doc with new option and add seed to test function to avoid to generete different image every time the doc is compiled
    
    * Change authors order
    
    ---------
    
    Co-authored-by: Enrico Stragiotti <enrico.stragiotti@onera.fr>

--- smt/sampling_methods/random.py
+++ smt/sampling_methods/random.py
@@ -6,12 +6,53 @@ This package is distributed under New BSD license.
 Random sampling.
 """
 
+import warnings
+
 import numpy as np
 
 from smt.sampling_methods.sampling_method import ScaledSamplingMethod
 
+# Check NumPy version
+numpy_version = tuple(
+    map(int, np.__version__.split(".")[:2])
+)  # Extract major and minor version
+
 
 class Random(ScaledSamplingMethod):
+    def _initialize(self, **kwargs):
+        self.options.declare(
+            "random_state",
+            types=(type(None), int, np.random.RandomState, np.random.Generator),
+            desc="Numpy RandomState or Generator object or seed number which controls random draws",
+        )
+
+        # Update options values passed by the user here to get 'random_state' option
+        self.options.update(kwargs)
+
+        # RandomState and Generator are and have to be initialized once at constructor time,
+        # not in _compute to avoid yielding the same dataset again and again
+        if numpy_version < (2, 0):  # Version is below 2.0.0
+            if isinstance(self.options["random_state"], np.random.RandomState):
+                self.random_state = self.options["random_state"]
+            elif isinstance(self.options["random_state"], np.random.Generator):
+                self.random_state = np.random.RandomState()
+                warnings.warn(
+                    "numpy.random.Generator initialization of random_state is not implemented for numpy "
+                    "versions < 2.0.0. Using the default np.random.RandomState() as random_state. "
+                    "Please consider upgrading to numpy version > 2.0.0, or use the legacy numpy.random.RandomState "
+                    "class in the future.",
+                    FutureWarning,
+                )
+            elif isinstance(self.options["random_state"], int):
+                self.random_state = np.random.RandomState(self.options["random_state"])
+            else:
+                self.random_state = np.random.RandomState()
+        else:
+            # Construct a new Generator with the default BitGenerator (PCG64).
+            # If passed a Generator, it will be returned unaltered. When passed a legacy
+            # RandomState instance it will be coerced to a Generator.
+            self.random_state = np.random.default_rng(seed=self.options["random_state"])
+
     def _compute(self, nt):
         """
         Implemented by sampling methods to compute the requested number of sampling points.
@@ -30,4 +71,9 @@ class Random(ScaledSamplingMethod):
         """
         xlimits = self.options["xlimits"]
         nx = xlimits.shape[0]
-        return np.random.rand(nt, nx)
+        if numpy_version < (2, 0):  # Version is below 2.0.0
+            return self.random_state.rand(nt, nx)
+        else:
+            # Create a Generator object with a specified seed (numpy.random_state.rand(nt, nx)
+            # is being deprecated)
+            return self.random_state.random((nt, nx))
