db588671fccf66bb5952ba795144508142f12ff6
relf <remi.lafage@onera.fr>
2026-02-11 22:14:03 +0100

    Refactor project structure: migrate metadata to pyproject.toml, remove setup.cfg, and clean up setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,6 @@
-"""
-Author: John T. Hwang <hwangjt@umich.edu>
-        Mohamed A. Bouhlel <mbouhlel@umich.edu>
-        Remi Lafage <remi.lafage@onera.fr>
-        Lucas Alber <lucasd.alber@gmail.com>
-        Paul Saves <paul.saves@onera.fr>
+"""Minimal setup.py for building Cython/C++ extensions.
 
-This package is distributed under New BSD license.
+All project metadata is in pyproject.toml.
 """
 
 import sys
@@ -13,42 +8,6 @@ import sys
 import numpy as np
 from Cython.Build import cythonize
 from setuptools import Extension, setup
-
-from smt import __version__
-
-CLASSIFIERS = """\
-Development Status :: 5 - Production/Stable
-Intended Audience :: Science/Research
-Intended Audience :: Developers
-License :: OSI Approved :: BSD License
-Programming Language :: C++
-Programming Language :: Python
-Programming Language :: Python :: 3
-Programming Language :: Python :: 3.9
-Programming Language :: Python :: Implementation :: CPython
-Topic :: Software Development
-Topic :: Scientific/Engineering
-Operating System :: Microsoft :: Windows
-Operating System :: Unix
-Operating System :: MacOS
-"""
-
-LONG_DESCRIPTION = """
-The surrogate modeling toolbox (SMT) is a Python package that contains \
-a collection of surrogate modeling methods, sampling techniques, and \
-benchmarking functions. This package provides a library of surrogate \
-models that is simple to use and facilitates the implementation of additional methods. \
-
-SMT is different from existing surrogate modeling libraries because of \
-its emphasis on derivatives, including training derivatives used for \
-gradient-enhanced modeling, prediction derivatives, and derivatives \
-with respect to the training data. It also includes new surrogate models \
-that are not available elsewhere: kriging by partial-least squares reduction \
-and energy-minimizing spline interpolation.
-
-SMT 2.0 adds the capability to handle mixed-variable surrogate models \
-and hierarchical variables.
-"""
 
 extra_compile_args = []
 if not sys.platform.startswith("win"):
@@ -90,45 +49,4 @@ ext = (
     )
 )
 
-
-metadata = dict(
-    name="smt",
-    version=__version__,
-    description="The Surrogate Modeling Toolbox (SMT)",
-    long_description=LONG_DESCRIPTION,
-    author="Remi Lafage et al.",
-    author_email="remi.lafage@onera.fr",
-    license="BSD-3",
-    classifiers=[_f for _f in CLASSIFIERS.split("\n") if _f],
-    packages=[
-        "smt",
-        "smt.surrogate_models",
-        "smt.problems",
-        "smt.sampling_methods",
-        "smt.utils",
-        "smt.applications",
-        "smt.applications.tests",
-        "smt.design_space",
-        "smt.kernels",
-    ],
-    install_requires=[
-        "packaging",
-        "scikit-learn",
-        "pyDOE3 >= 1.5.0",
-        "scipy",
-        "jenn >= 2.0.0, <3.0",
-    ],
-    extras_require={
-        "numba": [  # pip install smt[numba]
-            "numba~=0.56.4",
-        ],
-        "gpx": ["egobox~=0.34"],  # pip install smt[gpx]
-    },
-    python_requires=">=3.9",
-    zip_safe=False,
-    ext_modules=ext,
-    url="https://github.com/SMTorg/smt",  # use the URL to the github repo
-    download_url="https://github.com/SMTorg/smt/releases",
-)
-
-setup(**metadata)
+setup(ext_modules=ext)
